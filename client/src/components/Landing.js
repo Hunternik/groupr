fa5051116f67bf727abbe0466ff65df391ec9546
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-01-16 19:43:59 -0800

    removed temp landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,25 +1,10 @@
 import React, { Component } from 'react';
-import whiteboarding from "../assets/images/Wall-Sketching/MP4/Wall-Sketching.mp4";
 
 class Landing extends Component {
     render() {
         return (
-            <div class="homepage-hero-module">
-                <div class="video-container">
-                    <div class="title-container">
-                        <div class="headline">
-                            <h1>BasicReqMeetup</h1>
-
-                        </div>
-                        <div class="description">
-                            <div class="inner">Connecting quality candidates with quality companies</div>
-                        </div>
-                    </div>
-                    <div class="filter"></div>
-                    <video autoplay loop class="fillWidth">
-                        <source src={whiteboarding} type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
-                    </video>
-                </div>
+            <div>
+                <h1>Landing</h1>
             </div>
         );
     }
