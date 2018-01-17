dc3c069eea1b5ac71c22c9d8d07d06e0d643795d
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-01-16 16:12:36 -0800

    set up node server and react server

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,12 @@
 // Login Routes
 module.exports = () => {
-
     app.get("/", function(req, res) {
         res.send({ hi: "there" });
     });
 
+    // in the profile response we have access to: 
+    //  id, name, displayName, birthday, relationship, isPerso, isPlusUser, placesLived, language, emails, gender, picture;
+    app.get("/auth/google", passport.authenticate("google", {
+        scope: ["profile", "email"]   
+    }));
 }
