a4f203634f65a28bc3b125b11b8aedf75a5e8af8
james <jmsjtu@gmail.com>
2018-01-18 11:02:14 -0800

    fixed an issue where auth was not redirecting properly

--- apis/users_api.js
+++ apis/users_api.js
@@ -3,13 +3,9 @@ const passport = require('passport');
 // Login authentication handlers
 
 // Request authentication
-module.exports.requestToken = (req, res) => {
-  passport.authenticate('google', {
-    scope: [ 'profile', 'email' ]
-  });
-};
+module.exports.requestToken = passport.authenticate('google', {
+  scope: [ 'profile', 'email' ]
+});
 
 // callback route after passport authenticates the first time, run a 2nd time with the token
-module.exports.authenticateUser = (req, res) => {
-  passport.authenticate('google', (req, res) => {});
-};
+module.exports.authenticateUser = passport.authenticate('google', (req, res) => {});
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const authRoutes = require('./authRoutes');
 
 module.exports = (app) => {
-	app.use('/', (req, res) => res.json({hi: 'there'}))
 	app.use('/auth', authRoutes);
 };
