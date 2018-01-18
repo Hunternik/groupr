d81be365dfe79bbb9daf592fc6d25a50c1e9516a
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-01-18 14:45:39 -0800

    prevented duplicated users in db

--- services/passport.js
+++ services/passport.js
@@ -14,10 +14,16 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            done(null, existingUser);
+          } else {
+            new User({ googleId: profile.id }).save()
+              .then(user => done(null, user));
+          }
+        });
+
     }
   )
 );
