5407af22e715c89332be45f010792460923066e4
ykeanu <ykhernan@ucla.edu>
2018-01-17 18:01:31 -0800

    EventPage component

---
+++ client/src/components/EventPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EventPage extends Component {
+  render(){
+    return (
+      <div>
+          Event Information
+      </div>
+    );
+  }
+}
+
+export default EventPage;
