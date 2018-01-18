bf1bb3ac4b89160ac445b505d3a9aa3e09c366e0
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-01-18 13:49:22 -0800

    set up mlab, user model schema

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
 const TotpStrategy = require('passport-totp').Strategy;
+const mongoose = require("mongoose");
 const keys = require('../config/keys');
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -14,17 +17,39 @@ passport.use(
       console.log('accessToken', accessToken);
       console.log('refreshToken', refreshToken);
       console.log('profile', profile);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
 
-passport.use(
-  new TotpStrategy(function(user, done) {
-    TotpKey.findOne({ userId: user.id }, function(err, key) {
-      if (err) {
-        return done(err);
-      }
-      return done(null, key.key, key.period);
-    });
-  })
-);
+// Linkedin Login info, will set up later
+// passport.use(
+//   new LinkedInStrategy(
+//     {
+//       clientID: LINKEDIN_KEY,
+//       clientSecret: LINKEDIN_SECRET,
+//       callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback",
+//       scope: ["r_emailaddress", "r_basicprofile"]
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       process.nextTick(function() {
+//         // To keep the example simple, the user's LinkedIn profile is returned to
+//         // represent the logged-in user. In a typical application, you would want
+//         // to associate the LinkedIn account with a user record in your database,
+//         // and return that user instead.
+//         return done(null, profile);
+//       });
+//     }
+//   )
+// );
+
+// passport.use(
+//   new TotpStrategy(function(user, done) {
+//     TotpKey.findOne({ userId: user.id }, function(err, key) {
+//       if (err) {
+//         return done(err);
+//       }
+//       return done(null, key.key, key.period);
+//     });
+//   })
+// );
