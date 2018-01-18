c2762d313534a246d9c631c072c6c8e80791c503
james <jmsjtu@gmail.com>
2018-01-17 18:25:25 -0800

    set up initial modularization of auth routes on server

--- services/passport.js
+++ services/passport.js
@@ -4,15 +4,18 @@ const TotpStrategy = require('passport-totp').Strategy;
 const keys = require('../config/keys');
 
 passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        console.log("accessToken", accessToken);
-        console.log("refreshToken", refreshToken);
-        console.log("profile", profile);
-    })
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
+    }
+  )
 );
 
 passport.use(
