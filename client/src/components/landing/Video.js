86db36dc25f17d8b238948d5be839ac0a274c32d
james <jmsjtu@gmail.com>
2018-01-18 00:14:40 -0800

    set up client side routing

--- client/src/components/landing/Video.js
+++ client/src/components/landing/Video.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
 
 const Video = () => {
 	return (
-		<div>
+		<Container>
 			<video src='https://secure.meetupstatic.com/s/img/457419671895069178/guest_home/video.mp4' autoPlay loop></video>
-		</div>
+		</Container>
 	);
 }
 
