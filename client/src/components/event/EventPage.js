3bfdcbd7a47ff68068885497a28b249f3cf8a4e7
ykeanu <ykhernan@ucla.edu>
2018-01-18 18:14:04 -0800

    Added Jumbotron component

--- client/src/components/event/EventPage.js
+++ client/src/components/event/EventPage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Container, Header, Button, Icon } from 'semantic-ui-react';
+
+import Jumbotron from './Jumbotron';
 
 class EventPage extends Component {
   render(){
     return (
       <div>
-          Event Information
+        <Jumbotron />
       </div>
     );
   }
