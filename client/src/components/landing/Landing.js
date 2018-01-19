5f57ad7f55a9f981e8e901760f4c8cba0633f3c9
james <jmsjtu@gmail.com>
2018-01-18 18:09:04 -0800

    initial changes

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { Divider, Visibility } from 'semantic-ui-react';
 import Video from './Video';
+import Events from './Events';
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <Video />
-      </div>
+			<Visibility>
+				<div>
+					<Video />
+				</div>
+				<Divider horizontal inverted />
+				<div>
+					<Events />
+				</div>
+			</Visibility>
     );
   }
 }
--- client/src/components/landing/events.js
+++ client/src/components/landing/events.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Container, Header, Image, List } from 'semantic-ui-react';
+import Images from '../../constants/eventPreview';
+require('./landing.css');
 
-// const renderEvents = (events) => {
-// 	return events.map(event => <li>{event}</li>)
-// }
+const renderImages = () =>
+  Images.map((image, index) => (
+    <List.Item>
+      <Image src={image.src} />
+      <Header className='caption' color='white' size="large">{image.title}</Header>
+    </List.Item>
+  ));
 
 const Events = () => {
-	return (
-		<ul>
-			
-		</ul>
-	);
-}
+  return (
+    <Container>
+      <List>{renderImages()}</List>
+    </Container>
+  );
+};
+
+export default Events;
