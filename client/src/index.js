7e6ae314c18560b16f9d8e5a6bb7a2a9bc05a54d
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-01-20 15:52:08 -0800

    added heroku production build

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
