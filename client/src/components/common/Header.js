0e7a04adae0fb5501a04935bcfead763d389cf2b
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-01-21 12:08:57 -0800

    set up login/out button on header

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,39 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Divider, Dropdown, Grid, Header, Image, List, Menu, Segment } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const FixedMenuLayout = () => (
-  <div>
-    <Menu fixed="top" inverted>
-      <Container>
-        <Menu.Item as="a" header>
-          <Image size="mini" src="/logo.png" style={{ marginRight: '1.5em' }} />
-          Project Name
-        </Menu.Item>
-        <Menu.Item as={Link} to="/">Home</Menu.Item>
-        {/* Temporary for development */}
-        <Menu.Item as={Link} to="/event-page">Event Page (test)</Menu.Item>
+class FixedMenuLayout extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <a href="/auth/logout">Log Out</a>;
+      default:
+        return <a href="/auth/google">Login</a>;
+    }
+  }
 
-        <Dropdown item simple text="Dropdown">
-          <Dropdown.Menu>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Header>Header Item</Dropdown.Header>
-            <Dropdown.Item>
-              <i className="dropdown icon" />
-              <span className="text">Submenu</span>
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <Menu fixed="top" inverted>
+          <Container>
+            <Menu.Item as="a" header>
+              <Image size="mini" src="/logo.png" style={{ marginRight: '1.5em' }} />
+              Project Name
+            </Menu.Item>
+            <Menu.Item as={Link} to="/">Home</Menu.Item>
+            {/* Temporary for development */}
+            <Menu.Item as={Link} to="/event-page">Event Page (test)</Menu.Item>
+            <Menu.Item>
+              {this.renderContent()}
+            </Menu.Item> 
+            <Dropdown item simple text="Dropdown">
               <Dropdown.Menu>
                 <Dropdown.Item>List Item</Dropdown.Item>
                 <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Header>Header Item</Dropdown.Header>
+                <Dropdown.Item>
+                  <i className="dropdown icon" />
+                  <span className="text">Submenu</span>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>List Item</Dropdown.Item>
+                    <Dropdown.Item>List Item</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown.Item>
+                <Dropdown.Item>List Item</Dropdown.Item>
               </Dropdown.Menu>
-            </Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Container>
-    </Menu>
-  </div>
-);
+            </Dropdown>
+          </Container>
+        </Menu>
+      </div>
+    );
+  }
+}
 
-export default FixedMenuLayout;
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+
+
+export default connect(mapStateToProps)(FixedMenuLayout);
