094462452b8108c9917a815d8c0a9d65773928cb
Mix Master Mike <mikeyamato@gmail.com>
2018-01-22 16:34:49 -0800

    updated logout route and included a way to select user upon login

--- apis/users_api.js
+++ apis/users_api.js
@@ -5,24 +5,19 @@ const passport = require('passport');
 // Request authentication
 // ********* Google OAuth *********	
 module.exports.requestGoogleToken = passport.authenticate('google', {
-  scope: [ 'profile', 'email' ]
+  scope: [ 'profile', 'email' ],
+  prompt: "select_account"
 });
 
 // callback route after passport authenticates the first time, run a 2nd time with the token
-// module.exports.authenticateUser = [ passport.authenticate('google'), (req, res) => res.redirect('/') ];
-// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-// mike: revised below. why include the 2nd part above?
-// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
 module.exports.authenticateGoogleUser = [ passport.authenticate('google'), (req, res) => res.redirect('/') ];
 
 
 
 // ********* LinkedIn OAuth *********
 module.exports.requestLinkedInToken =  passport.authenticate('linkedin', {
-  scope: ['r_basicprofile', 'r_emailaddress']
+  scope: ['r_basicprofile', 'r_emailaddress'],
+  prompt: "select_account"
 });
 
 module.exports.authenticateLinkedInUser = [ passport.authenticate('linkedin'), (req, res) => res.redirect('/') ];
-// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-// mike: do we need to include the 2nd part?
-// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
