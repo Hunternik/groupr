2d88f246d12f5f80d4374db5b5cabd6d34d20240
Grace Park <gracepark@github.com>
2018-01-27 15:41:41 -0800

    remove head in types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,7 +1,4 @@
 export const FETCH_USER = 'fetch_user';
 export const SCROLL = 'scroll';
-<<<<<<< HEAD
 export const FETCH_QUIZ = 'fetch_quiz';
-=======
 export const FETCH_EVENT = 'fetch_event';
->>>>>>> master
