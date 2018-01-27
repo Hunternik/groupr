cf16224cdb64c21f423b4acff08b38aa2f793543
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-01-27 15:23:20 -0800

    created quiz component

---
+++ client/src/components/quiz/Quiz.js
@@ -0,0 +1,13 @@
+import react, { Component } from 'react';
+
+class Quiz extends Component { 
+    render() {
+        return (
+            <div>
+                Hi Grace
+            </div>
+        );
+    }
+}
+
+export default Quiz;
