283288d5a12311f639d0ddd58177d479ec4e6c57
ykeanu <ykhernan@ucla.edu>
2018-01-27 18:08:00 -0800

    Added details + Wire up event components to redux

--- client/src/components/event/Description.js
+++ client/src/components/event/Description.js
@@ -7,37 +7,25 @@ import {
 } from 'semantic-ui-react';
 
 class Description extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
   render() {
     return (
       <div>
-        <Segment style={{ padding: '8em 0em' }} vertical>
-          <Grid container stackable verticalAlign='middle'>
-            <Grid.Row>
-              <Grid.Column width={9}>
-                <Header as='h3' style={{ fontSize: '2em' }}>
-                  Description
-                </Header>
-                <p style={{ fontSize: '1.33em' }}>
-                  Doors open from 11-4PM. Come early to get free swag gear!
-                </p>
-                <Header as='h3' style={{ fontSize: '2em' }}>
-                  Companies Attending
-                </Header>
-                <p style={{ fontSize: '1.33em' }}>
-                  Google, Snapchat
-                </p>
-              </Grid.Column>
-              <Grid.Column floated='right' width={5}>
-                <Image
-                  bordered
-                  rounded
-                  size='large'
-                  src='https://i.stack.imgur.com/dApg7.png'
-                />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Segment>
+        <Header as='h3' style={{ fontSize: '2em' }}>
+          Description
+        </Header>
+        <p style={{ fontSize: '1.33em' }}>
+          {this.props.event.description}
+        </p>
+        <Header as='h3' style={{ fontSize: '2em' }}>
+          Companies Attending
+        </Header>
+        <p style={{ fontSize: '1.33em' }}>
+          Google, Snapchat
+        </p>
       </div>
     )
   }
--- client/src/components/event/EventPage.js
+++ client/src/components/event/EventPage.js
@@ -1,37 +1,60 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { 
+  Segment,
+  Grid,
+  Header, 
+  Image
+} from 'semantic-ui-react';
 // Actions
 import { fetchEvent } from '../../actions';
 // Components
 import Jumbotron from './Jumbotron';
 import Description from './Description';
+import Details from './Details';
 import Participants from './Participants';
 
 class EventPage extends Component {
   constructor(props) {
     super(props);
     this.fetchCurrentEvent = this.fetchCurrentEvent.bind(this);
+    this.renderEventData = this.renderEventData.bind(this);
   }
   
   // Component state
-  // state = {
-  // }
+  state = {}
 
   componentDidMount() {
     this.fetchCurrentEvent();
   }
 
-  // Fetch event data from database
+  // Fetch event data from mongo
   fetchCurrentEvent() {
-    console.log("ITS WORKING");
-    this.props.fetchEvent({});
+    this.props.fetchEvent();
+  }
+
+  renderEventData() {
+    console.log(this.props.event, "EVENT OBJECT")
+    return this.props.event;
   }
 
   render() {
     return (
       <div>
-        <Jumbotron />
-        <Description />
+        <h1>{this.renderEventData().title}</h1>
+        <Jumbotron event={this.renderEventData()} />
+        <Segment style={{ padding: '8em 0em' }} vertical>
+          <Grid container stackable verticalAlign='middle'>
+            <Grid.Row>
+              <Grid.Column width={9}>
+              <Description event={this.renderEventData()} />
+              </Grid.Column>
+              <Grid.Column floated='right' width={5}>
+                <Details event={this.renderEventData()}  />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
         <Participants />
       </div>
     );
@@ -40,7 +63,6 @@ class EventPage extends Component {
 
 // Application State
 const mapStateToProps = ({ event }) => ({
-  // event: state.event
   event
 });
 
