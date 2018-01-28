ee86ea4941e609329818445234f16243579aba78
Grace Park <gracepark@github.com>
2018-01-28 14:47:15 -0800

    question and answer are up

--- client/src/components/quiz/Quiz.js
+++ client/src/components/quiz/Quiz.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Form, Radio } from 'semantic-ui-react';
 import * as actions from '../../actions';
 
 class Quiz extends Component {
+  constructor() {
+    super();
+
+    this.getAnswers = this.getAnswers.bind(this);
+  }
   componentDidMount() {
     this.props.fetch_quiz();
   }
 
+  getAnswers(answers) {
+    console.log(this.props);
+    if (this.props.quiz) {
+      return Object.entries(this.props.quiz.num1.answers).map(
+        ([key, value], i) => {
+          return (
+            <div key={key}>
+              {key}. {value.toString()}
+            </div>
+          );
+        }
+      );
+    }
+  }
+
   render() {
     const quiz = this.props.quiz ? this.props.quiz.num1 : 'null';
-    return <div>{quiz.question}</div>;
+    console.log(quiz.answers);
+    return (
+      <div>
+        <div>{quiz.question}</div>
+        <div>{quiz && this.getAnswers(quiz.answers)}</div>
+      </div>
+    );
   }
 }
 
