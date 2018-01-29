7715277b494ca301eab0010eb90f880d0fdbc829
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-01-28 20:43:04 -0800

    created Correct Page

---
+++ client/src/components/quiz/CorrectPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CorrectPage extends Component {
+  render()  {
+    return (
+      <h1>Correct Answers!</h1>
+    );
+  }
+}
+
+export default CorrectPage;
