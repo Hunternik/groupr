0309b37d2e87568f98d16571fde9cadac2ecd418
Grace Park <gracepark@github.com>
2018-01-28 16:18:24 -0800

    added quiz index

--- client/src/components/quiz/Quiz.js
+++ client/src/components/quiz/Quiz.js
@@ -6,17 +6,55 @@ import * as actions from '../../actions';
 class Quiz extends Component {
   constructor() {
     super();
-
+    this.state = {
+      index: 0,
+      complete: false,
+      answer: null,
+      score: 0
+    };
+    this.nextQuestion = this.nextQuestion.bind(this);
+    this.correctAnswer = this.correctAnswer.bind(this);
+    this.wrongAnswer = this.wrongAnswer.bind(this);
     this.getAnswers = this.getAnswers.bind(this);
   }
+
+  correctAnswer() {
+    this.setState({
+      index: 0,
+      answer: true,
+      score: this.state.score + 1
+    });
+  }
+
+  wrongAnswer() {
+    this.setState({
+      answer: false
+    });
+  }
+
+  nextQuestion() {
+    const length = Object.keys(this.props.quiz.questions).length;
+    const { index } = this.state;
+    if (index === length - 1) {
+      this.setState({
+        complete: true
+      });
+    } else {
+      this.setState({
+        index: this.state.index + 1,
+        answer: null
+      });
+    }
+  }
+
   componentDidMount() {
     this.props.fetch_quiz();
   }
 
   getAnswers(answers) {
-    console.log(this.props);
+    const { index } = this.state;
     if (this.props.quiz) {
-      return Object.entries(this.props.quiz.num1.answers).map(
+      return Object.entries(this.props.quiz.questions[index].answers).map(
         ([key, value], i) => {
           return (
             <div key={key}>
@@ -29,11 +67,20 @@ class Quiz extends Component {
   }
 
   render() {
-    const quiz = this.props.quiz ? this.props.quiz.num1 : 'null';
-    console.log(quiz.answers);
+    const { index } = this.state;
+    const quiz = this.props.quiz ? this.props.quiz.questions : 'null';
+    const currentQuestion = quiz[index].question;
+    console.log(index);
+
+    // if (this.props.quiz) {
+    //   this.nextQuestion();
+    // } else {
+    //   return null;
+    // }
+
     return (
       <div>
-        <div>{quiz.question}</div>
+        <div>{currentQuestion}</div>
         <div>{quiz && this.getAnswers(quiz.answers)}</div>
       </div>
     );
