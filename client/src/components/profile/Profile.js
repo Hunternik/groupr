02710ed3acee6b80074e48c83a86b1515ab588ce
Grace Park <gracepark@github.com>
2018-01-31 00:15:37 -0800

    basic redux form up

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 import {
   Button,
   Container,
@@ -8,40 +9,90 @@ import {
   Grid,
   Segment
 } from 'semantic-ui-react';
-import { reduxForm, Field } from 'react-redux-form';
 import { connect } from 'react-redux';
 require('./profile.css');
+
+const required = value => (value ? undefined : 'Required');
+const email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    ? 'Invalid email address'
+    : undefined;
+const maxLength = max => value =>
+  value && value.length > max ? `Must be ${max} characters or less` : undefined;
+const maxLength25 = maxLength(25);
+export const minLength = min => value =>
+  value && value.length < min ? `Must be ${min} characters or more` : undefined;
+export const minLength2 = minLength(2);
+const alphaNumeric = value =>
+  value && /[^a-zA-Z0-9 ]/i.test(value)
+    ? 'Only alphanumeric characters'
+    : undefined;
+const renderField = ({
+  input,
+  label,
+  type,
+  meta: { touched, error, warning }
+}) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type} />
+      {touched &&
+        ((error && <span>{error}</span>) ||
+          (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+);
 
 class Profile extends Component {
   renderForm() {
     return (
       <Form>
-        <Form.Field>
-          <label>First Name</label>
-          <input placeholder="First Name" />
-        </Form.Field>
-        <Form.Field>
-          <label>Last Name</label>
-          <input placeholder="Last Name" />
-        </Form.Field>
-        <Form.Field>
-          <label>Email</label>
-          <input placeholder="Email" />
-        </Form.Field>
-        <Form.Field>
-          <label>Company</label>
-          <input placeholder="Company" />
-        </Form.Field>
-        <Form.Field>
-          <label>Position</label>
-          <input placeholder="Position" />
-        </Form.Field>
+        <Field
+          name="First Name"
+          type="text"
+          component={renderField}
+          label="First Name"
+          validate={[required, maxLength25, minLength2]}
+          warn={alphaNumeric}
+        />
+        <Field
+          name="Last Name"
+          type="text"
+          component={renderField}
+          label="Last Name"
+          validate={[required, maxLength25, minLength2]}
+          warn={alphaNumeric}
+        />
+        <Field
+          name="email"
+          type="email"
+          component={renderField}
+          label="Email"
+          validate={email}
+        />
+        <Field
+          name="Company"
+          type="text"
+          component={renderField}
+          label="Company"
+          validate={[required, maxLength25, minLength2]}
+          warn={alphaNumeric}
+        />
+        <Field
+          name="Position"
+          type="text"
+          component={renderField}
+          label="Position"
+          validate={[required, maxLength25, minLength2]}
+          warn={alphaNumeric}
+        />
         <div className="button-group">
           <Button className="profile-button" size="large">
-            Update
-          </Button>
-          <Button className="profile-button" size="large">
             Cancel
+          </Button>
+          <Button type="submit" className="profile-button" size="large">
+            Update
           </Button>
         </div>
       </Form>
@@ -49,7 +100,7 @@ class Profile extends Component {
   }
 
   render() {
-    console.log(this.props);
+    const { handleSubmit, pristine, reset, submitting } = this.props;
     return (
       <Container>
         <Header as="h1" textAlign="center">
@@ -75,4 +126,7 @@ class Profile extends Component {
 
 const mapStateToProps = ({ auth }) => ({ auth });
 
-export default connect(mapStateToProps)(Profile);
+Profile = reduxForm({ form: 'Profile' })(Profile);
+Profile = connect(mapStateToProps)(Profile);
+
+export default Profile;
