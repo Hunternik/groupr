8f370360098d4a3efb9a6c9569d0a139c330b094
Grace Park <gracepark@github.com>
2018-01-30 21:05:36 -0800

    add getCompany

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const companies_api = require('../apis/companies_api');
 
-// ********* Sponsor Route *********	
-router.post('/:id', companies_api.getEvent);
+// ********* Sponsor Route *********
+router.post('/:id', companies_api.getCompany);
 
 module.exports = router;
