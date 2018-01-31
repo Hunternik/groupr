d9cd2c5dc6df41e4d4930c7f8beb7fdb7b424c53
Grace Park <gracepark@github.com>
2018-01-30 20:29:57 -0800

    fix for now

--- models/Companies.js
+++ models/Companies.js
@@ -3,12 +3,12 @@ const { Schema } = mongoose;
 const userSchema = require('./User');
 
 const companySchema = new Schema({
-	eventId: String,
-	name: String,
-	industry: String,
-	website: String,
-	employees: [userSchema],
-	events: [];
+  eventId: String,
+  name: String,
+  industry: String,
+  website: String,
+  employees: [userSchema],
+  events: []
 });
 
 // first arugment passed into models is the name of the collection, second arg is info
