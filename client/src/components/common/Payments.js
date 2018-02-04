64375f23b611e723216c5ee143a6f27d79d8600d
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-02-03 18:17:43 -0800

    playing around w stripe

--- client/src/components/common/Payments.js
+++ client/src/components/common/Payments.js
@@ -19,6 +19,7 @@ class Payments extends Component {
 				name='Grouper'
 				description='$1 for 1 credit to make you holla'
 				allowRememberMe='false'
+				closed={console.log("OHMYGOSHTHISSHITISCLOSING")}
 			>
 			<div>
     			<Button color='teal' size='large'>
