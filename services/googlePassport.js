f2d850d7cf0635119324fb6794c626d17d190767
james <jmsjtu@gmail.com>
2018-02-05 17:40:43 -0800

    refactored passport to keep more dry

--- services/googlePassport.js
+++ services/googlePassport.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose");
 const keys = require("../config/keys");
 const User = mongoose.model("users");
 const { serialize, deserialize } = require("./utils/serialize");
+const googleService = require('./utils/serviceHandler');
 
 passport.serializeUser(serialize);
 
@@ -20,45 +21,7 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
       proxy: true
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      let existingUser = await User.findOne({ googleId: profile.id });
-
-      if (existingUser) {
-        return done(null, existingUser);
-      }
-
-      const googleUser = {
-        googleId: profile.id,
-        displayName: profile.displayName,
-        lastName: profile.name.familyName,
-        firstName: profile.name.givenName,
-        email: profile.emails[0].value,
-        iconPhotoURL: profile.photos[0].value,
-				bigPhotoURL: profile._json.cover.coverPhoto.url,
-				googleProfileURL: profile._json.url
-      };
-
-      // Check to see if user already exists by comparing email addresses
-      existingUser = await User.findOne({ email: googleUser.email });
-
-      // If email exists, update user record with new authentication ID
-      if (existingUser) {
-				existingUser.googleId = googleUser.googleId;
-				existingUser.googleProfileURL = googleUser.googleProfileURL
-
-        try {
-					existingUser = await existingUser.save();
-					
-          return done(null, existingUser);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-
-			// Create a new user record is user does not exist
-      const user = await new User(googleUser).save();
-      done(null, user);
-    }
+		},
+		googleService.serviceHandler
   )
 );
--- services/linkedinPassport.js
+++ services/linkedinPassport.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const keys = require("../config/keys");
 const User = mongoose.model("users");
 const { serialize, deserialize } = require("./utils/serialize");
-
+const linkedInService = require('./utils/serviceHandler');
 
 passport.serializeUser(serialize);
 
@@ -22,47 +22,7 @@ passport.use(
       callbackURL: "/auth/linkedin/callback",
       scope: ["r_basicprofile", "r_emailaddress"],
       proxy: true
-    },
-    async (accessToken, refreshToken, profile, done) => {
-			console.log(profile);
-      // Search for user by authentication ID
-      let existingUser = await User.findOne({ linkedInId: profile.id });
-
-      if (existingUser) {
-        return done(null, existingUser);
-      }
-
-      const linkedinUser = {
-        linkedInId: profile.id,
-        displayName: profile.displayName,
-        lastName: profile.name.familyName,
-        firstName: profile.name.givenName,
-        email: profile.emails[0].value,
-        iconPhotoURL: profile.photos[0].value,
-				bigPhotoURL: profile._json.pictureUrls.values[0],
-				linkedInProfileURL: profile._json.publicProfileUrl
-      };
-
-      // Check to see if user already exists by comparing email addresses
-      existingUser = await User.findOne({ email: linkedinUser.email });
-
-      // If email exists, update user record with new authentication ID
-      if (existingUser) {
-				existingUser.linkedInId = linkedinUser.linkedInId;
-				existingUser.linkedInProfileURL = linkedinUser.linkedInProfileURL;
-
-        try {
-          existingUser = await existingUser.save();
-
-          return done(null, existingUser);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-
-			// Create a new user record is user does not exist
-      const user = await User(linkedinUser).save();
-      done(null, user);
-    }
+		},
+		linkedInService.serviceHandler
   )
 );
