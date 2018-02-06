b902a5b7a1b50b7ee059eba7628a368789228ad2
james <jmsjtu@gmail.com>
2018-02-05 20:25:03 -0800

    fixed an issue where login was not working correctly

--- services/utils/googleApi.js
+++ services/utils/googleApi.js
@@ -1,4 +1,5 @@
 module.exports.googleUser = profile => {
+	const image = profile.photos[0].value.slice(0,profile.photos[0].value.indexOf('?sz=50'));
   const googleUser = {
     identifier: "googleId",
     profileUrlId: "googleProfileURL",
@@ -8,8 +9,8 @@ module.exports.googleUser = profile => {
       lastName: profile.name.familyName,
       firstName: profile.name.givenName,
       email: profile.emails[0].value,
-      iconPhotoURL: profile.photos[0].value,
-      bigPhotoURL: profile._json.cover.coverPhoto.url,
+      iconPhotoURL: `${image}?sz=50`,
+      bigPhotoURL: `${image}?sz=250`,
       googleProfileURL: profile._json.url
     }
   };
