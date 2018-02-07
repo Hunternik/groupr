6ab2731ebebbb0a758c4f62968be6afee80b0784
ykeanu <ykhernan@ucla.edu>
2018-02-06 23:53:33 -0800

    Added input and additional styling to rendered profile components

--- client/src/components/event/Participants.js
+++ client/src/components/event/Participants.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Segment, Grid, Menu, Label, Image, Card } from 'semantic-ui-react';
+import { Segment, Grid, Menu, Label, Image, Card, Input } from 'semantic-ui-react';
 // Actions
 import { fetchEvent, fetchEventSponsors } from '../../actions';
 
@@ -24,7 +24,7 @@ class Participants extends Component {
     if (this.props.event.companies) {
       return this.props.event.companies.map(company => {
         return (
-            <Card color="teal" style={styles.card}>
+            <Card>
               <Image src={company.imgLogoURL} fluid rounded />
             </Card>
         );
@@ -63,7 +63,7 @@ class Participants extends Component {
       <div>
         <Grid container>
           <Grid.Column width={3}>
-            <Menu fluid vertical tabular>
+            <Menu vertical>
               <Menu.Item
                 name="companies"
                 active={activeItem === 'companies'}
@@ -88,10 +88,13 @@ class Participants extends Component {
                 <Label>128</Label>
                 Attendies
               </Menu.Item>
+              <Menu.Item>
+          <Input icon='search' placeholder='Search mail...' />
+        </Menu.Item>
             </Menu>
           </Grid.Column>
           <Grid.Column stretched width={7}>
-            <Segment>
+            <Segment style={styles.card}>
               <Card.Group itemsPerRow={4}>
                 {activeItem === 'companies' && this.renderEventCompanies()}
                 {activeItem === 'recruiters' && this.renderEventRecruiters()}
@@ -109,6 +112,10 @@ const styles = {
   card: {
     backgroundColor: '#f6f7f8',
     boxShadow: '0 0 0 0px #d4d4d5, 0 0px 0 0 #00b5ad, 0 1px 3px 0 #d4d4d5'
+  },
+  cardCompanies: {
+    backgroundColor: '#f6f7f8',
+    boxShadow: '0 0 0 0px #d4d4d5, 0 0px 0 0 #00b5ad, 0 0px 0px 0 #d4d4d5'
   }
 };
 
