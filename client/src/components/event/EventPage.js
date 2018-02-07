33451a1a7d94c2a7bdfddd118e4df1067e3480bd
ykeanu <ykhernan@ucla.edu>
2018-02-06 23:19:59 -0800

    Removed vertical borders on segments for event page

--- client/src/components/event/EventPage.js
+++ client/src/components/event/EventPage.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Segment, Grid, Header, Image, Rail, Sticky } from 'semantic-ui-react';
+import { Segment, Grid, Header, Image, Rail, Sticky, Divider } from 'semantic-ui-react';
 import { fetchEvent, fetchEventSponsors } from '../../actions';
 import Jumbotron from './Jumbotron';
 import Description from './Description';
@@ -56,7 +56,7 @@ class EventPage extends Component {
           event={this.renderEventData()}
           coverPhotoID={this.props.match.params.eventId.toUpperCase()}
         />
-        <Segment style={{ padding: '0em 0em' }} vertical>
+        <Segment style={{ padding: '0em 0em' }} basic>
           <Grid container centered columns={2}>
             <Grid.Column width={10}>
               <Description event={this.renderEventData()} />
@@ -66,9 +66,11 @@ class EventPage extends Component {
             </Grid.Column>
           </Grid>
         </Segment>
-        <Segment vertical>
+        <Divider hidden />
+        <Segment basic>
           <Participants eventId={this.props.event} />
         </Segment>
+        <Divider hidden />
       </div>
     );
   }
