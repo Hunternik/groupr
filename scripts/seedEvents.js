85cd7188553375fab55170a06ccdb47c36a7d3e4
ykeanu <ykhernan@ucla.edu>
2018-02-07 19:24:19 -0800

    Refactor event seed for mongoDB

--- scripts/seedEvents.js
+++ scripts/seedEvents.js
@@ -9,9 +9,9 @@ module.exports = async () => {
 	const eventSeed = [
 		{
 			eventId: 'ATX',
-			title: 'Girls in Tech Austin',
+			title: 'Girls in Tech ATX',
 			description:
-				'Come enjoy an evening with Girls in Tech! \r\n We invite you to our monthly networking dinner to meet new people around the Bay Area. Whether you are looking to hire brilliant talent for your new startup or want to learn from experts in different industries, come out to our networking dinner and make some new friends. You will connect with tons of people and learn about all the awesome projects and events going on around the Bay. \r\n People of all backgrounds and genders are welcome to attend. Be sure to reserve your seat by purchasing your ticket here (dinner and drinks included).',
+				'Come enjoy an evening with Girls in Tech! \r\n We invite you to our monthly networking dinner to meet new people around the Texas. Whether you are looking to hire brilliant talent for your new startup or want to learn from experts in different industries, come out to our networking dinner and make some new friends. You will connect with tons of people and learn about all the awesome projects and events going on around the Bay. \r\n People of all backgrounds and genders are welcome to attend. Be sure to reserve your seat by purchasing your ticket here (dinner and drinks included).',
 			date: new Date(Date.now()),
 			location: '804 Congress Avenue, Austin, TX 78701',
 			attendees: [],
@@ -23,7 +23,7 @@ module.exports = async () => {
 		},
 		{
 			eventId: 'LA',
-			title: 'TechFest Los Angeles',
+			title: 'TechFest LA',
 			description:
 				"LA's Largest Technology Job Fair will be returning to LA for an even bigger and better event for the second year in a row, co-hosted by Comparably, Annenberg Foundation and Office of Mayor Eric Garcetti. Over 250 most prominent tech companies in attendence along with 10,000+ job-seekers in Engineering, Product, Sales, Marketing, Design and more attending.",
 			date: new Date(Date.now()),
@@ -51,7 +51,7 @@ module.exports = async () => {
 		},
 		{
 			eventId: 'SF',
-			title: 'Speed Recruiting in San Francisco',
+			title: 'Speed Recruiting Silicon Valley',
 			description:
 				'Are you looking for a job or looking to transition into a new career? Would you like to get tips and advice to help you land your dream job? Join us for our Speed Networking/Mentoring event where you will get face time and job search advice from recruiters, hiring managers and career counselors. \r\n The evening will start with lightning talks by each recruiter followed by the speed networking. Jobseekers will then meet recruiters, hiring managers and career counselors in a group mentoring format, asking questions and advice about your job search and making valuable connections. The groups will switch every 10 minutes.',
 			date: new Date(Date.now()),
@@ -79,7 +79,7 @@ module.exports = async () => {
 		},
 		{
 			eventId: 'SD',
-			title: 'San Diego Startup and Tech',
+			title: 'Super Start-Up La Jolla',
 			description:
 				'San Diego Startup & Tech is the fastest growing startup, digital & tech networking organization in the country. Our events are comprised of the most influential professionals in the area. Our purpose is to help our members forge strong, lasting relationships within the community combining Fortune 500 & 1000s with Entrepreneurs and Startups. We are creating unique opportunities & connections to help you grow your business and network! \r\n Enjoy The Most Successful Startup & Tech Mixer In A Fun, Relaxed Atmosphere \r\n Mingle With Highly Successful Men & Women In The Entrepreneur, Startup, TECH Space \r\n Find Your Next Client, Employee, Business Partner or Co-founder \r\n Your Network Is Your Net Worth. Come Build Your Network With Us!',
 			date: new Date(Date.now()),
@@ -93,9 +93,9 @@ module.exports = async () => {
 		},
 		{
 			eventId: 'DET',
-			title: 'HackerX Detroit',
+			title: 'HackerFest Detroit',
 			description:
-				'You are invited to HackerX, an invite-only recruiting event for developers. It is a one-of-a-kind opportunity to meet with and learn from some of the most innovative and fastest growing companies in a casual environment.',
+				'You are invited to HackerFest, an invite-only recruiting event for developers. It is a one-of-a-kind opportunity to meet with and learn from some of the most innovative and fastest growing companies in a casual environment.',
 			date: new Date(Date.now()),
 			location: '1520 Woodward Ave #500 Detroit, MI 48226',
 			attendees: [],
@@ -107,7 +107,7 @@ module.exports = async () => {
 		},
 		{
 			eventId: 'DEN',
-			title: 'WomenHack Denver',
+			title: 'WomenHackX Denver',
 			description:
 				"You're invited to WomenHack, an invite-only recruiting event for women developers, designers, and product managers. Whether you are actively looking for your next challenge, or are curious about the hiring landscape, attending our events can help you gain an understanding of the market dynamics and see which companies will fit your requirements and support the growth of your career.",
 			date: new Date(Date.now()),
@@ -135,7 +135,7 @@ module.exports = async () => {
 		},
 		{
 			eventId: 'WAS',
-			title: 'Speeding Mentoring in DC',
+			title: 'Speed Mentoring for Tech DC',
 			description:
 				'Speed mentoring is a super casual and fun way for people to meet others in their field and learn more about what’s needed to grow. We’ll have 3-4 sessions throughout the night and you can sign up to be in 1 or all 4. Each session is 30-45 minutes. We’ll guide & prompt you and in each session you’ll get to provide wisdom to multiple mentees. You feel good, they feel good. Win-win.',
 			date: new Date(Date.now()),
