5b9ed0237ac6b5cb504a825b15c6df232cec599d
james <jmsjtu@gmail.com>
2018-02-07 23:27:13 -0800

    updated seed file for admins

--- scripts/seedUserInactiveEvent.js
+++ scripts/seedUserInactiveEvent.js
@@ -6,25 +6,18 @@ const mongoURI = process.argv[2] === "prod" ? keys.mongoURIPROD : keys.mongoURI;
 mongoose.connect(mongoURI);
 
 (async () => {
-  const User = await db.User.findOne({ _id: "5a7be829f309f59f5f5f392e" });
-  User.events = [];
-
-  try {
-    await User.save();
-    console.log("user save successful!");
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
+	const userEmail = ['jmsjtu@gmail.com'];
   const inactiveEvent = await db.Event.find({ active: false });
   const activeEvent = await db.Event.findOne({ active: true });
   const allEvents = [...inactiveEvent, activeEvent];
-
-  User.events = allEvents.map(event => event._id);
+  const eventIds = allEvents.map(event => event._id);
 
   try {
-    await User.save();
+    const data = await db.User.update(
+			{ email: { $in: userEmail } },
+			{ $set: { events: eventIds } },
+      { multi: true }
+		)
     console.log("successfully added events");
     process.exit(0);
   } catch (err) {
