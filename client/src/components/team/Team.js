208c5f42434945ef258686de81f952c384ada9e1
james <jmsjtu@gmail.com>
2018-02-09 00:29:17 -0800

    resolved issue with team info not appearing correctly

--- client/src/components/team/Team.js
+++ client/src/components/team/Team.js
@@ -3,9 +3,34 @@ import { Card, Image, Icon, Container, Transition } from 'semantic-ui-react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
+import Cards from '../../constants/teamInfo';
 import './team.css';
 
 class Team extends Component {
+	renderCards() {
+		return Cards.map(card => (
+			<Card className="card-container">
+				<Image 
+					className="card-image"
+					src={card.image} />
+				<Card.Content>
+					<Card.Header>{card.name}</Card.Header>
+					<Card.Meta>
+						<span className="date">{card.title}</span>
+					</Card.Meta>
+					<Card.Description>
+						{card.description}
+					</Card.Description>
+				</Card.Content>
+				<Card.Content extra>
+					<a href={card.url} />
+					<Icon name="code" />
+						GitHub
+					</Card.Content>
+      </Card>
+		));
+	}
+
   render() {
     var settings = {
       dots: true,
@@ -39,100 +64,7 @@ class Team extends Component {
           </div>
           <div className="gallery">
             <Slider {...settings}>
-              <Card>
-                <Image src="../../assets/images/ProfileImages/james.png" />
-                <Card.Content>
-                  <Card.Header>James</Card.Header>
-                  <Card.Meta>
-                    <span className="date">Software Engineer</span>
-                  </Card.Meta>
-                  <Card.Description>
-                    James is a software engineer that loves his dog Blue, shoes,
-                    and playing computer games.
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <Image src="https://github.com/jmsjtu" />
-                  <Icon name="code" />
-                  GitHub
-                </Card.Content>
-              </Card>
-              <Card>
-                <Image src="https://avatars1.githubusercontent.com/u/22055162?s=460&v=4" />
-                <Card.Content>
-                  <Card.Header>Grace</Card.Header>
-                  <Card.Meta>
-                    <span className="date">Software Engineer</span>
-                  </Card.Meta>
-                  <Card.Description>
-                    Grace is a software engineer and former actor that loves her
-                    dog Mandu, whiskey, and traveling.
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <a href="https://github.com/gracepark">
-                    <Icon name="code" />
-                    GitHub
-                  </a>
-                </Card.Content>
-              </Card>
-              <Card>
-                <Image src="https://avatars3.githubusercontent.com/u/27989364?s=400&v=4" />
-                <Card.Content>
-                  <Card.Header>Donovan</Card.Header>
-                  <Card.Meta>
-                    <span className="date">Software Engineer</span>
-                  </Card.Meta>
-                  <Card.Description>
-                    Donovan is a software engineer that loves climbing,
-                    skateboarding, and being outdoors.
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <a href="https://github.com/dlowkeen">
-                    <Icon name="code" />
-                    GitHub
-                  </a>
-                </Card.Content>
-              </Card>
-              <Card>
-                <Image src="https://avatars0.githubusercontent.com/u/27756216?s=460&v=4" />
-                <Card.Content>
-                  <Card.Header>Mike</Card.Header>
-                  <Card.Meta>
-                    <span className="date">Software Engineer</span>
-                  </Card.Meta>
-                  <Card.Description>
-                    Mike is a software engineer with years of digital marketing
-                    experience and loves BBC.
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <a href="https://github.com/mikeyamato">
-                    <Icon name="code" />
-                    GitHub
-                  </a>
-                </Card.Content>
-              </Card>
-              <Card>
-                <Image src="https://avatars0.githubusercontent.com/u/28130499?s=460&v=4" />
-                <Card.Content>
-                  <Card.Header>Izzy</Card.Header>
-                  <Card.Meta>
-                    <span className="date">Software Engineer</span>
-                  </Card.Meta>
-                  <Card.Description>
-                    Izzy is a software engineer and former gymnast that loves
-                    playing basketball and music.
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <a href="https://github.com/ykeanu">
-                    <Icon name="code" />
-                    GitHub
-                  </a>
-                </Card.Content>
-              </Card>
+							{this.renderCards()}
             </Slider>
           </div>
         </Container>
---
+++ client/src/constants/teamInfo.js
@@ -0,0 +1,42 @@
+export default [
+	{
+		name: 'James',
+		title: 'Software Engineer',
+		description: `James is a software engineer that loves his dog Blue, shoes,
+		and playing computer games.`,
+		url: 'https://github.com/jmsjtu',
+		image: `https://avatars0.githubusercontent.com/u/8368171?s=400&u=7a335948c4bbe0483288c741cf4b0aeba3007065&v=4`
+	},
+	{
+		name: 'Grace',
+		title: 'Software Engineer',
+		description: `Grace is a software engineer and former actor that loves her
+		dog Mandu, whiskey, and traveling.`,
+		url: 'https://github.com/gracepark',
+		image: `https://avatars1.githubusercontent.com/u/22055162?s=460&v=4`
+	},
+	{
+		name: 'Donovan',
+		title: 'Software Engineer',
+		description: `Donovan is a software engineer that loves climbing,
+		skateboarding, and being outdoors.`,
+		url: 'https://github.com/dlowkeen',
+		image: `https://avatars3.githubusercontent.com/u/27989364?s=400&v=4`
+	},
+	{
+		name: 'Mike',
+		title: 'Software Engineer',
+		description: `Mike is a software engineer with years of digital marketing
+		experience and loves BBC.`,
+		url: 'https://github.com/mikeyamato',
+		image: `https://avatars0.githubusercontent.com/u/27756216?s=460&v=4`
+	},
+	{
+		name: 'Izzy',
+		title: 'Software Engineer',
+		description: `Izzy is a software engineer and former gymnast that loves
+		playing basketball and music.`,
+		url: 'https://github.com/ykeanu',
+		image: `https://avatars0.githubusercontent.com/u/28130499?s=460&v=4`
+	},
+]
