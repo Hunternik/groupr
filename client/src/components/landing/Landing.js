a4c00afbe7ce289a3a79952dd3d8bd1d2c56ecc9
james <jmsjtu@gmail.com>
2018-02-08 17:36:26 -0800

    set resolution for video

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -26,7 +26,7 @@ class Landing extends Component {
 
   render() {
     return (
-      <Visibility style={{ height: '100%' }}>
+      <Visibility>
         <Video />
         <Divider horizontal inverted />
         <About />
