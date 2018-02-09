791987c27e24daba139aad28ad2cf8c9c30838ac
ykeanu <ykhernan@ucla.edu>
2018-02-08 19:51:35 -0800

    Hotfix newrelix

--- newrelic.js
+++ newrelic.js
@@ -16,7 +16,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: keys.newRelicLicenseKey,
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
