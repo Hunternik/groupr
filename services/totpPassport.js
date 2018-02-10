d1a72fa2620f8d2906dc5afd744ec23799d62f99
james <jmsjtu@gmail.com>
2018-02-09 19:58:46 -0800

    removing console.logs

--- services/totpPassport.js
+++ services/totpPassport.js
@@ -14,19 +14,10 @@ passport.use(
 	'totp', new TotpStrategy(
     function (user, done) {
     // setup function, supply key and period to done callback
-    console.log("********* TotpStrategy - user: ", user);
-    console.log("********* TotpStrategy - done 1: ", done);
     Totp.findOne({_id: user.id}, (err, obj) => {
-      console.log("********* TotpStrategy - id: ", {_id: user.id});
-      console.log("********* TotpStrategy - obj: ", obj);
-      console.log('********* TotpStrategy - obj.key: ', obj.key);
-      console.log('********* TotpStrategy - obj.period: ', obj.period);
-      //console.log('********* TotpStrategy - done 2: ', done(null, obj.key, obj.period));
       if (err) {
-        console.log('********* TotpStrategy - errr: ', err)
         return done(err);
 			}
-      // console.log('********* TotpStrategy - done: ', done(null, obj.key, obj.period))
       return done(null, obj.key, obj.period);
     });
   }
